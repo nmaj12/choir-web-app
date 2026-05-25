64eb17d2c64c0564c24cb77d00d1c0c614a63c15
Michu3224 <michalos.krol@gmail.com>
2026-05-25 09:31:55 +0200

    Dodanie Post

---
+++ choir_app/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace choir_app.Models
+{
+    public class Post
+    {
+        //komentarz
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tytuł jest wymagany")]
+        [Display(Name = "Tytuł wpisu")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Treść wpisu jest wymagana")]
+        [Display(Name = "Treść")]
+        public string Content { get; set; }
+
+        [Display(Name = "Data publikacji")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [Display(Name = "Kategoria")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+    }
+}
